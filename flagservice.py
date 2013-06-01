1f30679ed6e0ca4f491a2cf7d0d037f8bb49a7f1
John O'Neil <joneil@joneil-miniserv.joneil.dyndns.org>
2013-06-01 03:56:14 -0700

    moved server to a more RESTful architecture. Api described in comments.

--- flagservice.py
+++ flagservice.py
@@ -6,6 +6,24 @@ Desc:
 Author: John O'Neil
 Email:
 
+  This is an attempt at a simple RESTful api
+  to provide little flags for IRC users (just associate
+  countries and flag images with irc nicks). I'm not
+  building in security (no passwords) so anyone should
+  be able to reassign someone's flag (i don't care really,
+  it's just an experiment).
+  it should work as follows:
+  1)Request a list of countries (returned as json):
+  http://www.demo.com/flagservice/countries
+  2)Request the country associated with a nick (as json)
+  http://www.demo.com/flagservice/NICK/country
+  3)Request an image of the flag associated with a given nick
+  http://www.demo.com/flagservice/NICK/country/image
+  4)Requst an image of any country's flag
+  http://www.demo.com/flagservice/countries/XX/image (where XX is the ISOv2 country abbreviation)
+  5)Create a nick entry associated with a country, or update a nick's associated country:
+  http://www.demo.com/flagservice/NICK/?country=xx
+
 """
 #!/usr/bin/python
  
@@ -25,35 +43,99 @@ import re
 import glob
 import random
 import sys
+import FlagServiceDatabase
+import pycountry
 
-#Set up and parse command line options using Tornado wrappers
-#this is quick, but probably not advisable in the long term
-from tornado.options import define, options
-define("mysql_host", default="127.0.0.1", help="database host")
-define("mysql_database", default="irc", help="database name")
-define("mysql_user", default="ircuser", help="database user")
-define("mysql_password", default="", help="database password")
 
 class TestPage(tornado.web.RequestHandler):
-	def get(self):
-		self.render("test.html")
+  def post(self):
+    # nick = self.get_argument('nick','random', True)
+    # print "nick is :" + nick
+
+    #Pull an optional 'country' argument out of request
+    #if the country argument exists, this is an attempt to set
+    #a nick/country association
+    #country = self.get_argument('country',None)
+    #if(country):
+    #  print 'country is ' + country
+    #  iso_country = pycountry.countries.get(name=country)
+    #  if(iso_country):
+    #    print 'country iso name = ' + iso_country.alpha2
+    self.render("test.html")
+
+  def get(self):
+    self.render("test.html")
 
 class FlagRequestHandler(tornado.web.StaticFileHandler):
   def get(self):
     #pull the nick out of our request
     nick = self.get_argument('nick','random', True)
     print "nick is :" + nick
-    
+
     #fetch the flag assigned to this nick
     #if one isn't assigned a random one will be assigned
     flag = self.application.get_flag(nick)
     print 'assigned flag is :' + flag
-    return super(FlagRequestHandler, self).get(flag, True)   
+    return super(FlagRequestHandler, self).get(flag, True)
+  
+
+  def post(self):
+    nick = self.get_argument('nick','random', True)
+    print "nick is :" + nick
+
+    #Pull an optional 'country' argument out of request
+    #if the country argument exists, this is an attempt to set
+    #a nick/country association
+    country = self.get_argument('country',None)
+    if(country):
+      print 'country is ' + country
+      iso_country = pycountry.countries.get(name=country)
+      if(iso_country):
+        print 'country iso name = ' + iso_country.alpha2
+
+class ListCountries(tornado.web.RequestHandler):
+  def get(self, **params):
+    countries = {'countries':self.application.flags}
+    self.write(json.dumps((countries),sort_keys=True, indent=4))
+
+class GetFlagForCountry(tornado.web.StaticFileHandler):
+  def get(self, **params):
+    country = params['country']
+    print 'requesting flag for country :' + country
+    return super(GetFlagForCountry, self).get('/home/joneil/code/FlagImageService/image/' + country + '.gif', True)
+
+class GetCountryForNick(tornado.web.RequestHandler):
+  def get(self, **params):
+    nick = params['nick']
+    country = self.application.get_flag(nick)
+    print nick + ' has assigned country ' + country
+    assigned_country = {nick:country}
+    self.write(json.dumps(assigned_country,sort_keys=True, indent=4))
+    #self.write(country)
+
+class GetFlagForNick(tornado.web.StaticFileHandler):
+  def get(self, **params):
+    nick = params['nick']
+    country = self.application.get_flag(nick)
+    print nick + ' has assigned country ' + country
+    return super(GetFlagForNick, self).get('/home/joneil/code/FlagImageService/image/' + country + '.gif', True)
+
+class SetFlagForNick(tornado.web.RequestHandler):
+  def set(self, **params):
+    nick = params['nick']
+    country = params['country']
+    self.application.set_flag(nick,country)
+      
 
 class FlagWebService(tornado.web.Application):
   def __init__(self, image_path):
 
     handlers = [
+      (r'/countries', ListCountries),
+      (r'/countries/?(?P<country>[A-Za-z]{2})?/image',GetFlagForCountry, {'path': image_path}),
+      (r'/nick/?(?P<nick>[A-Za-z0-9-_]+)?/country',GetCountryForNick),
+      (r'/nick/?(?P<nick>[A-Za-z0-9-_]+)?/country/image',GetFlagForNick,{'path': image_path}),
+      (r'/nick/?(?P<nick>[A-Za-z0-9-_]+)?/country/?(?P<country>[A-Za-z]{2})?',SetFlagForNick),
       (r"/", FlagRequestHandler, {'path': image_path}),
       (r"/test",TestPage),
     ]
@@ -62,52 +144,16 @@ class FlagWebService(tornado.web.Application):
       debug=True,
     )
     super(FlagWebService,self).__init__(handlers,**settings)
-    
-    self.flags = glob.glob(settings['image_path'] +'/*.gif')
-    self.database = sql.connect(options.mysql_host, options.mysql_user, 
-      options.mysql_password, options.mysql_database,use_unicode=1,charset="utf8")
-
-    self.CreateTableIfNoneExists()
-
-  def CreateTableIfNoneExists(self):
-    cursor =  self.database.cursor()
-    if cursor is not None:
-      cursor.execute( 'CREATE TABLE IF NOT EXISTS flags \
-      (\
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,\
-        timestamp TIMESTAMP DEFAULT NOW(),\
-        created TIMESTAMP,\
-        nick VARCHAR(4096),\
-        flag VARCHAR(4096)\
-        );')
-      result = cursor.fetchall()
-      cursor.close()
-
-  def ReadFlag(self, nick):
-    cursor = self.database.cursor()
-    if(cursor is not None):
-      query = 'SELECT * FROM flags WHERE nick = \'{0}\''.format(nick)
-      print query
-      rows = cursor.execute(query)
-      result = cursor.fetchone()
-      if result:
-        print result
-        return result[4]
-      cursor.close()
-    return None
-    
-  def WriteFlag(self, nick, flag):
-    cursor = self.database.cursor()
-    if( cursor is not None):
-      sqlcommand = 'INSERT INTO flags(nick, flag)\
-        VALUES (\'{0}\', \'{1}\');'.format(nick, flag)
-      print sqlcommand
-      cursor.execute( sqlcommand )
-      #result = cursor.fetchall()
-      cursor.close()
+    self.flags = filter(lambda x: x.endswith('.gif'), os.listdir(settings['image_path']))
+    self.flags = [ os.path.splitext(e)[0] for e in self.flags ]
+    #self.flags = [ os.path.splitext(e)[0] for e in glob.glob(settings['image_path'] +'/*.gif')]
+    self.database = FlagServiceDatabase.FlagServiceDatabase()
+    self.database.CreateTableIfNoneExists()
 
   def get_flag(self, nick):
-    flag = self.ReadFlag(nick)
+    if nick == 'random':
+      return random.choice(self.flags)
+    flag = self.database.ReadFlag(nick)
     if flag is None:
       return self.set_flag(nick)
     else:
@@ -116,7 +162,7 @@ class FlagWebService(tornado.web.Application):
   def set_flag(self, nick, flag = None):
     if flag is None:
       flag = random.choice(self.flags)
-      self.WriteFlag(nick, flag)
+      self.database.WriteFlag(nick, flag)
     #todo: see if it's a valid flag
     return flag
 
@@ -135,7 +181,7 @@ def main():
 
   finally:
     if application:
-      database.close()
+      application.database.close()
  
 if __name__ == "__main__":
   main()
